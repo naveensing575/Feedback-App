9a144d8d4ac7b0e772d42350ffbd57568d58fcfb
naveensing575 <naveensing575@gmail.com>
2022-08-08 13:24:30 +0530

    update, delete, fetch added from json-server

--- src/Components/Context/FeedbackContext.js
+++ src/Components/Context/FeedbackContext.js
@@ -1,12 +1,10 @@
 import {useState, createContext, useEffect} from 'react';
-import {v4 as uuidv4} from 'uuid';
 
 const  FeedbackContext = createContext()
 
 export const FeedbackProvider=({children})=>{
   const [loading, setLoading] = useState(true);
   const [Feedback, setFeedback] = useState([])
-
   const [FeedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
@@ -19,28 +17,38 @@ export const FeedbackProvider=({children})=>{
   //Fetching Feedback from json-server
 
   const FetchFeedback = async() => {
-    const response = await fetch('http://localhost:3001/feedback')
+    const response = await fetch(`http://localhost:3001/feedback?_sort=id&_order=desc`)
     const data=await response.json(response);
-    console.log(data)
+    setFeedback(data)
     setLoading(false);
   }
 
   //Deleting Feedback
-  const deleteFeedback=(id)=>{
+  const deleteFeedback= async(id)=>{
     if (window.confirm('Are you sure, you want to delete this component?'))
     {
+      await fetch(`http://localhost:3001/feedback/${id}`,{ method: 'DELETE'})
       setFeedback(Feedback.filter((item)=>item.id !== id))
     }
   }
 
   //Add feedback
-  const addFeedback=(newFeedback)=>{
-    newFeedback.id=uuidv4();
-    setFeedback([newFeedback, ...Feedback])
+  const addFeedback= async(newFeedback)=>{
+    const response= await fetch(`http://localhost:3001/feedback`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(newFeedback),
+    })
+    const data = await response.json();
+
+    setFeedback([data, ...Feedback])
   }
 
   //Update Feedback
-  const UpdateFeedback=(id, updItem)=>{
+  const UpdateFeedback= async(id, updItem)=>{
+    await fetch(`http://localhost:3001/feedback/${id}`,{ method: 'PATCH'})
     setFeedback(Feedback.map((item)=>item.id === id?{...item, ...updItem}:item))
   }
 
